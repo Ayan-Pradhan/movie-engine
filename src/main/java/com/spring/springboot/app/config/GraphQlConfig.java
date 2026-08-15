424ab8b19bd3a0def21667205c4a3a4344691cc6
AYAN PRADHAN <ayanpradhan101@gmail.com>
2026-08-16 00:10:59 +0530

    config(GraphQL): add custom scalar wiring logic

--- src/main/java/com/spring/springboot/app/config/GraphQlConfig.java
+++ src/main/java/com/spring/springboot/app/config/GraphQlConfig.java
@@ -5,14 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
 
 import graphql.scalars.ExtendedScalars;
+import graphql.schema.GraphQLScalarType;
 
 @Configuration
 public class GraphQlConfig {
 	
 	@Bean
 	RuntimeWiringConfigurer runtimeWiringConfigurer() {
+		
+		GraphQLScalarType doubleScalar = GraphQLScalarType.newScalar(ExtendedScalars.GraphQLBigDecimal)
+				.name("Double")
+				.build();
+		
 		return wiringBuilder -> wiringBuilder
-				.scalar(ExtendedScalars.GraphQLBigDecimal);
+				.scalar(doubleScalar);
 	}
 
 }
