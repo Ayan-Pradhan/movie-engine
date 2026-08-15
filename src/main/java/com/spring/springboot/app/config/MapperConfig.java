ca47660110698a48416090251d19526b625a146d
AYAN PRADHAN <ayanpradhan101@gmail.com>
2026-08-15 23:54:17 +0530

    config(mapper): add ModelMapper config

---
+++ src/main/java/com/spring/springboot/app/config/MapperConfig.java
@@ -0,0 +1,21 @@
+package com.spring.springboot.app.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration
+public class MapperConfig {
+
+	@Bean
+	ModelMapper modelMapper() {
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration()
+			.setSkipNullEnabled(true)
+			.setFieldMatchingEnabled(true)
+			.setFieldAccessLevel(Configuration.AccessLevel.PRIVATE);
+		
+		return mapper;
+	}
+	
+}
