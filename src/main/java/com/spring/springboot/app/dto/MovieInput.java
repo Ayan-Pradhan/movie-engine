a1fd60b5a2bb8347afec0f21d2f7116905dee96e
AYAN PRADHAN <ayanpradhan101@gmail.com>
2026-08-17 22:44:35 +0530

    refactor(movie-input): change datatype to String from list

--- src/main/java/com/spring/springboot/app/dto/MovieInput.java
+++ src/main/java/com/spring/springboot/app/dto/MovieInput.java
@@ -1,12 +1,10 @@
 package com.spring.springboot.app.dto;
 
-import java.util.List;
-
 public record MovieInput(
 		String mid,
 		String movieName,
 		String runtime,
 		Double ratings,
-		List<String> language,
+		String language,
 		String description
 		) {}
