9c79b364aae0747e7d58eeb9498f8868962723bf
AYAN PRADHAN <ayanpradhan101@gmail.com>
2026-08-17 22:51:37 +0530

    .

---
+++ src/main/java/com/spring/springboot/app/exception/handler/MovieEngineExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.spring.springboot.app.exception.handler;
+
+import java.util.Map;
+
+import org.springframework.graphql.data.method.annotation.GraphQlExceptionHandler;
+import org.springframework.graphql.execution.ErrorType;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import com.spring.springboot.app.constant.ResponseCode;
+import com.spring.springboot.app.exception.MovieNotFoundException;
+
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+
+@ControllerAdvice
+public class MovieEngineExceptionHandler {
+
+	@GraphQlExceptionHandler(MovieNotFoundException.class)
+	public GraphQLError handleMovieNotFound(GraphqlErrorBuilder<?> errorBuilder, MovieNotFoundException ex) {
+		return errorBuilder
+				.errorType(ErrorType.NOT_FOUND)
+				.message(ex.getMessage())
+				.extensions(Map.of(
+						"code", ResponseCode.NOT_FOUND.toString()
+						))
+				.build();
+	}
+	
+	@GraphQlExceptionHandler(Exception.class)
+	public GraphQLError handleError(GraphqlErrorBuilder<?> errorBuilder, Exception ex) {
+		return errorBuilder
+				.errorType(ErrorType.INTERNAL_ERROR)
+				.message(ex.getMessage())
+				.extensions(Map.of(
+						"code", ResponseCode.ERROR.toString()
+						))
+				.build();
+	}
+	
+
+	
+	
+
+}
